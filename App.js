35d4022488c8caabb05c5ae219971b3dbba57bbe
bagauovkarim <159759615+bagauovkarim@users.noreply.github.com>
2024-02-15 06:21:09 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,63 @@
-import './App.css'
-import React from 'react';
-
+import "./App.css"
+import React, {useState, useEffect} from "react";
+//import ReactDOM from "react-dom";    
 
 
 function App() {
+    const [userData, setUserData] = useState([]);
+    const getApiData = async () => {
+        const response = await fetch(
+            "https://dummyjson.com/users"
+        ).then((response) => response.json());
+        setUserData(response.users);
+        
+        
+    };
+    
+    useEffect(() => {
+        getApiData();
+        
+        
+    }, []);
+
+    
+    console.log(userData);
+    
+
     
   return (
       <div className = "wrapper">
           <div className = "content">
                 <div className = "dataTable">
                   <table>
-                      <th>N</th>
-                      <th>Fullname</th>
-                      <th>Age</th>
-                      <th>Gender</th>
-                      <th>Telephone</th>
-                      <th>Address</th>
-                      <tbody>
+                      <thead className = "head">
+                          <tr>
+                              <th className="h1"> id </th>
+                              <th className="h2"> firstName </th>
+                              <th className="h3"> lastName </th>
+                              <th className="h4"> maidenName </th>
+                              <th className="h5"> age </th>
+                              <th className="h6"> gender </th>
+                              <th className="h7"> phone </th>
+                              <th className="h8"> email </th>
 
+                          </tr>
+                      </thead>
+                      
+                      <tbody Name = "body">
+                          {userData.map((user)  => (
+                          <tr className = "tablerow">
+                             <td className = "id"> {user.id}</td>
+                             <td className = "firstName">{user.firstName}</td>
+                             <td className = "lastName">{user.lastName}</td>
+                             <td className = "maidenName">{user.maidenName}</td>
+                             <td className = "age">{user.age}</td>
+                             <td className = "gender">{user.gender}</td>
+                             <td className = "email">{user.email}</td>
+                             <td className = "phone">{user.phone}</td>
+                          </tr>
+                          ))}  
+                          
 
                       </tbody>
                   </table>
@@ -33,28 +73,9 @@ function App() {
 
       </div>
 
+
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 export default App;
+
+
